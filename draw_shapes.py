c383950c5e27056893072ccf59e3c96f198a5e77
Simon Anders <sanders@fs.tum.de>
2026-01-01 12:40:52 +0100

    Create draw_shapes.py

---
+++ draw_shapes.py
@@ -0,0 +1,26 @@
+import glob, pathlib, pickle
+import czifile
+import napari
+
+all_shapes = dict()
+
+for filename in glob.glob("./*czi"):
+
+    basename = pathlib.Path(filename).stem
+    print(basename)
+
+    img = czifile.imread(filename).squeeze()
+    viewer = napari.Viewer()
+    viewer.add_image(img.max(1), channel_axis=0, colormap=['red', 'green', 'blue', 'cyan'], name=basename )
+    shapes_layer = viewer.add_shapes()
+    napari.run()
+
+    print(shapes_layer.data)
+    print()
+
+    all_shapes[basename] = shapes_layer.data
+
+with open( "shapes.pckl", "wb" ) as f:
+    pickle.dump( all_shapes, f )
+
+print( "Written shapes to 'shapes.pckl'." )
