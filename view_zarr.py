ee952a00a9367bcdd21968f508182b19259c3026
Simon Anders <sanders@fs.tum.de>
2026-01-02 23:38:27 +0100

    sys

--- view_zarr.py
+++ view_zarr.py
@@ -1,8 +1,9 @@
+import sys
 import napari
 import zarr
 
 viewer = napari.Viewer()
-data = zarr.open('merged_images.zarr', mode='r')
+data = zarr.open(sys.argv[1], mode='r')
 
 # Set contrast limits explicitly (adjust min/max to your data range)
 viewer.add_image(data['0'], channel_axis=0, contrast_limits=[0, 4095],
