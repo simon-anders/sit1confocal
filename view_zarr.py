22c2613700d35d484c764a3b5547180af160a871
Simon Anders <sanders@fs.tum.de>
2026-01-02 23:29:56 +0100

    3D

--- view_zarr.py
+++ view_zarr.py
@@ -7,6 +7,7 @@ data = zarr.open('merged_images.zarr', mode='r')
 # Set contrast limits explicitly (adjust min/max to your data range)
 viewer.add_image(data['0'], channel_axis=0, contrast_limits=[0, 4095],
   colormap=["magenta", "red", "green", "blue", "gray"], 
-  name=["actin","lyzome","granzyme","TCR","borders"] )
+  name=["phalloidin","lyzome","granzyme","TCR","borders"],
+  scale =[0.5, 0.03529, 0.03529]) 
 
 napari.run()
